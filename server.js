7d98f1e53ab98aafbd81b55b078038038f304102
Cmz <hollowmask18300@gmail.com>
2020-04-19 16:31:53 -0500

    insertion 4

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 //Install express server
 const express = require('express');
 const path = require('path');
- 
+
 const app = express();
  
 // Serve only the static files form the angularapp directory
@@ -9,8 +9,8 @@ app.use(express.static(__dirname + '/listar'));
  
 app.get('/*', function(req,res) {
  
-res.sendFile(path.join(__dirname+'/angularapp/index.html'));
+res.sendFile(path.join(__dirname+'/index.html'));
 });
- 
+
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
